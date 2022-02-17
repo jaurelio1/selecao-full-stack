6e8f015d8f3b01d017d1014b1a9d89f61f46ad27
jaec35515@gmail.com <jose.epaminondas@basis.com.br>
2022-02-17 16:16:54 -0300

    Concluido dashboard de Moedas

---
+++ selecao-beeteller-back/src/main/java/com/example/selecaobeetellerback/SelecaoBeetellerBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.selecaobeetellerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelecaoBeetellerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelecaoBeetellerBackApplication.class, args);
+	}
+
+}
